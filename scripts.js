79bab6056f025467c230b3d7e713a781019f4467
mhyneman8 <mallory.hyneman@gmail.com>
2022-04-25 11:45:20 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,246 @@
+//function section in Javascript and Jquery...
+function myData() {
+    // retrun;
+    return;
+  }
+  
+  function show() {
+    document.getElementById('anotherFunction').classList.toggle('Active');
+  }
+
+// modal show
+function showModal(id) {
+    document.getElementById(id).style.display="block";
+}
+
+// modal close
+function closeModal(id) {
+    document.getElementById(id).style.display="none";
+}
+
+// small menu
+
+function closeAnimation() {
+    a = document.querySelector('.row1');
+    b = document.querySelector('.row2');
+    c = document.querySelector('.row3'); 
+    d = document.querySelector('#hamburger');
+    menu = document.querySelector('#menu');
+    // let item = document.querySelectorAll('menu-item');
+
+    function toggleAnimation() {
+        a.classList.toggle('rotateRight');
+        b.classList.toggle('transparent');
+        c.classList.toggle('rotateLeft');
+        menu.classList.toggle('move'); 
+        // item.classList.toggle('slide')
+    };
+
+    d.addEventListener('click', function() {
+        toggleAnimation();
+    });
+} closeAnimation();
+  
+
+document.addEventListener("DOMContentLoaded", function () {
+    if (document.body.className === 'donuts-page') {
+        graph();
+    }
+})
+
+function graph() {
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+    google.charts.setOnLoadCallback(drawChart2);
+    google.charts.setOnLoadCallback(drawChart3);
+    google.charts.setOnLoadCallback(drawChart4);
+    google.charts.setOnLoadCallback(drawChart5);
+    google.charts.setOnLoadCallback(drawChart6);
+    google.charts.setOnLoadCallback(drawChart7);
+    google.charts.setOnLoadCallback(drawChart8);
+    google.charts.setOnLoadCallback(drawChart9);
+
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Work',     11],
+            ['Eat',      2],
+            ['Commute',  2],
+            ['Watch TV', 4]
+        ]);
+    
+        var options = {
+            width: '100%',
+            height: '100%',
+            legend: 'none',
+            backgroundColor: 'none',
+            chartArea: {width:'100%', height: '100%'},
+            colors: ['#f2ebe9', '#de5499', '#264143', '#e99f4c']
+        };
+    
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+        chart.draw(data, options);
+    }
+    
+    function drawChart2() {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Work',     11],
+            ['Commute',  2],
+            ['Watch TV', 2],
+            ['Sleep',    7]
+        ]);
+        var options = {
+            reverseCategories: true,
+            pieHole: 0.4,
+            width: '100%',
+            height: '100%',
+            legend: 'none',
+            backgroundColor: 'none',
+            chartArea: {width:'100%', height: '100%'},
+            colors: ['#f2ebe9', '#de5499', '#264143', '#e99f4c']
+        };
+        var chart2 = new google.visualization.PieChart(document.getElementById('donutchart'));
+        chart2.draw(data, options);
+    }
+    function drawChart3() {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Work',     11],
+            ['Commute',  2],
+            ['Watch TV', 2],
+            ['Sleep',    7]
+        ]);
+        var options = {
+            width: '100%',
+            height: '100%',
+            legend: 'none',
+            backgroundColor: 'none',
+            chartArea: {width:'100%', height: '100%'},
+            colors: ['#f2ebe9', '#de5499', '#264143', '#e99f4c']
+        };
+        var chart2 = new google.visualization.PieChart(document.getElementById('piechart2'));
+        chart2.draw(data, options);
+    }
+    function drawChart4() {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Eat',      2],
+            ['Commute',  2],
+            ['Watch TV', 2],
+            ['Sleep',    7]
+        ]);
+        var options = {
+            reverseCategories: true,
+            pidHole: 0.8,
+            width: '100%',
+            height: '100%',
+            legend: 'none',
+            backgroundColor: 'none',
+            chartArea: {width:'100%', height: '100%'},
+            colors: ['#f2ebe9', '#de5499', '#264143', '#e99f4c']
+        };
+        var chart2 = new google.visualization.PieChart(document.getElementById('donutchart2'));
+        chart2.draw(data, options);
+    }
+    function drawChart5() {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Work',     11],
+            ['Eat',      2],
+            ['Commute',  2],
+            ['Sleep',    7]
+        ]);
+        var options = {
+            width: '100%',
+            height: '100%',
+            legend: 'none',
+            backgroundColor: 'none',
+            chartArea: {width:'100%', height: '100%'},
+            colors: ['#f2ebe9', '#de5499', '#264143', '#e99f4c']
+        };
+        var chart2 = new google.visualization.PieChart(document.getElementById('piechart3'));
+        chart2.draw(data, options);
+    }
+    function drawChart6() {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Work',     11],
+            ['Eat',      2],
+            ['Commute',  2],
+            ['Watch TV', 2],
+        ]);
+        var options = {
+            reverseCategories: true,
+            pieHole: 0.3,
+            width: '100%',
+            height: '100%',
+            legend: 'none',
+            backgroundColor: 'none',
+            chartArea: {width:'100%', height: '100%'},
+            colors: ['#f2ebe9', '#de5499', '#264143', '#e99f4c']
+        };
+        var chart2 = new google.visualization.PieChart(document.getElementById('donutchart3'));
+        chart2.draw(data, options);
+    }
+    function drawChart7() {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Work',     11],
+            ['Commute',  2],
+            ['Watch TV', 2],
+            ['Sleep',    7]
+        ]);
+        var options = {
+            width: '100%',
+            height: '100%',
+            legend: 'none',
+            backgroundColor: 'none',
+            chartArea: {width:'100%', height: '100%'},
+            colors: ['#f2ebe9', '#de5499', '#264143', '#e99f4c']
+        };
+        var chart2 = new google.visualization.PieChart(document.getElementById('piechart4'));
+        chart2.draw(data, options);
+    }
+    function drawChart8() {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Eat',      2],
+            ['Commute',  2],
+            ['Watch TV', 2],
+            ['Sleep',    7]
+        ]);
+        var options = {
+            reverseCategories: true,
+            pieHole: 0.5,
+            width: '100%',
+            height: '100%',
+            legend: 'none',
+            backgroundColor: 'none',
+            chartArea: {width:'100%', height: '100%'},
+            colors: ['#f2ebe9', '#de5499', '#264143', '#e99f4c']
+        };
+        var chart2 = new google.visualization.PieChart(document.getElementById('donutchart4'));
+        chart2.draw(data, options);
+    }
+    function drawChart9() {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Work',     11],
+            ['Eat',      2],
+            ['Commute',  2],
+            ['Watch TV', 2]
+        ]);
+        var options = {
+            width: '100%',
+            height: '100%',
+            legend: 'none',
+            backgroundColor: 'none',
+            chartArea: {width:'100%', height: '100%'},
+            colors: ['#f2ebe9', '#de5499', '#264143', '#e99f4c']
+        };
+        var chart2 = new google.visualization.PieChart(document.getElementById('piechart5'));
+        chart2.draw(data, options);
+    }
+}
+
